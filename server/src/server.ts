9a73a4f322f82a58401aefe35fdfe1bdf3cd1657
Victor Brayner <victor.brayner@contasimples.com>
2023-05-19 10:03:48 -0300

    feat(Server): implements auth route with jwt token

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,21 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, { origin: true })
 
+app.register(jwt, { secret: 'spacetime' })
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
-  .listen({
-    port: 3333,
-  })
+  .listen({ port: 3333 })
   .then(() => console.log('HTTP Server running on http://localhost:3333'))
