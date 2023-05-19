69a38ee360855893e8f0a59e4a605c898bd83fa2
Victor Brayner <victor.brayner@contasimples.com>
2023-05-19 13:04:25 -0300

    fix(Server): add host to work with mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,5 +17,5 @@ app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
-  .listen({ port: 3333 })
+  .listen({ port: 3333, host: '0.0.0.0' })
   .then(() => console.log('HTTP Server running on http://localhost:3333'))
