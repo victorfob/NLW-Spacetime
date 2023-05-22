67b269df22b5c8a379ed29f22de072bb4208a78b
Victor Brayner <victor.brayner@contasimples.com>
2023-05-21 21:04:03 -0300

    feat(Server): create upload route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
 import 'dotenv/config'
 
+import { resolve } from 'node:path'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '..', 'uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, { origin: true })
 
@@ -15,6 +25,7 @@ app.register(jwt, { secret: 'spacetime' })
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({ port: 3333, host: '0.0.0.0' })
